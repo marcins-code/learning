a5f30311df6d28f5b6412329c246ee112ce4613e
marcins-code <mpaczkowski40@gmail.com>
2020-02-04 09:18:06 +0100

    Fixtures add

---
+++ src/DataFixtures/User.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Users;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+
+class User extends Fixture
+{
+    private $passwordEncoder;
+
+    public function __construct(UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $user = new Users();
+        $user->setUsername('paczkow');
+        $user->setPassword($this->passwordEncoder->encodePassword(
+            $user, 'ralf1996'
+        ));
+        $user->setRoles(['ROLE_ADMIN', 'ROLE_SUPER_USER']);
+        $user->setIsEnabled(true);
+
+        $manager->persist($user);
+        $manager->flush();
+
+    }
+}
